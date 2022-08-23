4082f77229861619527b4a3a29d5858ae8b65387
Evgenia0707 <4030071@gmail.com>
2022-08-23 16:46:45 -0400

    java14

---
+++ m8-new-version-updates/src/main/java/com/company/java14/User.java
@@ -0,0 +1,14 @@
+package com.company.java14;
+
+public record User(String name, String address) {
+
+
+    //Private Final, No Setters
+
+    //All ARG CONSTRUCTORS
+    //GETTER
+    //TO STRING
+    //EQUALS
+    //HASH CODE
+
+}
